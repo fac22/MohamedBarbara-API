6368c8d8cbd16c7a92f491df472af9ecc3c8a6ac
bubbles linux <appenzeller.barbara@gmail.com>
2021-08-10 16:07:47 +0100

    Script search-button, img size, content flex

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const imgFrame = document.querySelector("#img-frame");
-const imgPlholder = document.querySelector("#img-placeholder");
-const imgButton = document.querySelector("#img-button");
+const imgPlholder = document.querySelector('#img-placeholder');
+const imgButtonGenerate = document.querySelector('#img-generate');
+const imgButtonSearch = document.querySelector('#img-search');
+const imgWish = document.querySelector('#img-wish');
 
-imgButton.addEventListener("click", () => {
-  fetch("https://picsum.photos/200").then((response) => {
-    console.log(response.url);
-    imgPlholder.src = response.url;
-  });
+imgButtonGenerate.addEventListener('click', () => {
+	fetch('https://picsum.photos/200').then(response => {
+		console.log(response.url);
+		imgPlholder.src = response.url;
+	});
+});
+
+imgButtonSearch.addEventListener('click', () => {
+	console.log(imgWish.value);
+	// fetch('https://picsum.photos/200').then(response => {
+	// 	console.log(response.url);
+	// 	imgPlholder.src = response.url;
+	// });
+	fetch(`https://source.unsplash.com/weekly?${imgWish.value}`)
+		.then(response => {
+			if (!response.ok) throw new Error(response.status);
+			console.log(response);
+			console.log(response.url);
+			imgPlholder.src = response.url;
+		})
+		.catch(error => "Couldn't load image");
 });
