5193ef1b8272a056bb31f665c3f478463783a521
bubbles linux <appenzeller.barbara@gmail.com>
2021-08-10 16:45:47 +0100

    script addToGallery

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const imgPlholder = document.querySelector('#img-placeholder');
+const imgFrame = document.querySelector('#img-frame');
 const imgButtonGenerate = document.querySelector('#img-generate');
 const imgButtonSearch = document.querySelector('#img-search');
-const imgWish = document.querySelector('#img-wish');
+const imgWish = document.querySelector('#img-wish').value;
+
+console.log(imgWish);
+
+function addToGallery(url, alt) {
+	const image = document.createElement('img');
+	image.src = url;
+	image.alt = alt;
+	imgFrame.append(image);
+}
 
 imgButtonGenerate.addEventListener('click', () => {
-	fetch('https://picsum.photos/200').then(response => {
-		console.log(response.url);
-		imgPlholder.src = response.url;
-	});
+	fetch('https://picsum.photos/200')
+		.then(response => {
+			if (!response.ok) throw new Error(response.status);
+			console.log(response.url);
+			addToGallery(response.url, 'Random Unsplash image');
+		})
+		.catch(error => "Couldn't load image");
 });
 
 imgButtonSearch.addEventListener('click', () => {
-	console.log(imgWish.value);
-	// fetch('https://picsum.photos/200').then(response => {
-	// 	console.log(response.url);
-	// 	imgPlholder.src = response.url;
-	// });
-	fetch(`https://source.unsplash.com/weekly?${imgWish.value}`)
+	console.log(imgWish);
+	const wishValue = imgWish;
+	fetch(`https://source.unsplash.com/weekly?${wishValue}`)
 		.then(response => {
 			if (!response.ok) throw new Error(response.status);
-			console.log(response);
-			console.log(response.url);
-			imgPlholder.src = response.url;
+			addToGallery(response.url, `Image of ${wishValue}`);
 		})
 		.catch(error => "Couldn't load image");
 });
