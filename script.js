d7287e6ed944115d5aa7d83121fc0f908febc105
bubbles linux <appenzeller.barbara@gmail.com>
2021-08-11 16:30:37 +0100

    Comment out multiple image function

--- script.js
+++ script.js
@@ -7,29 +7,33 @@ const imgWish = document.querySelector('#img-wish');
 console.log(imgWish);
 
 function addToGallery(url, alt) {
-	const image = document.createElement('img');
-	image.src = url;
-	image.alt = alt;
-	imgFrame.append(image);
+  const image = document.createElement('img');
+  image.src = url;
+  image.alt = alt;
+  imgFrame.append(image);
 }
 
 imgButtonGenerate.addEventListener('click', () => {
-	fetch('https://picsum.photos/200')
-		.then(response => {
-			if (!response.ok) throw new Error(response.status);
-			console.log(response.url);
-			addToGallery(response.url, 'Random Unsplash image');
-		})
-		.catch(error => "Couldn't load image");
+  fetch('https://picsum.photos/200')
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      console.log(response.url);
+      // addToGallery(response.url, 'Random Unsplash image');
+      imgPlholder.src = response.url;
+      imgPlholder.alt = 'Random Unsplash image';
+    })
+    .catch((error) => "Couldn't load image");
 });
 
 imgButtonSearch.addEventListener('click', () => {
-	console.log(imgWish);
-	const wishValue = imgWish.value;
-	fetch(`https://source.unsplash.com/weekly?${wishValue}`)
-		.then(response => {
-			if (!response.ok) throw new Error(response.status);
-			addToGallery(response.url, `Image of ${wishValue}`);
-		})
-		.catch(error => "Couldn't load image");
+  console.log(imgWish);
+  const wishValue = imgWish.value;
+  fetch(`https://source.unsplash.com/weekly?${wishValue}`)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      // addToGallery(response.url, `Image of ${wishValue}`);
+      imgPlholder.src = response.url;
+      imgPlholder.alt = `Image of a ${wishValue}`;
+    })
+    .catch((error) => "Couldn't load image");
 });
